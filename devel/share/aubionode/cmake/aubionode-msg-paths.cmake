b4ae992f514f31ec07077bab0cd8a206bb9364e7
dave <dave@ubuntu.ubuntu-domain>
2015-06-12 11:56:27 -0400

    remove devel files from repo

--- devel/share/aubionode/cmake/aubionode-msg-paths.cmake
+++
@@ -1,5 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.em
-
-# message include dirs in develspace
-set(aubionode_MSG_INCLUDE_DIRS "")
-set(aubionode_MSG_DEPENDENCIES std_msgs;geometry_msgs)
